f0ad79d7ef2ae725378eeb7f6891e498f972a06c
kobi2210 <kobi_221@163.com>
2018-03-01 10:16:58 +0800

    test

--- mq/src/main/java/com/myproject/mq/App.java
+++ mq/src/main/java/com/myproject/mq/App.java
@@ -12,7 +12,7 @@ public class App
 {
     public static void main( String[] args )
     {
-		System.out.println("test");
+		System.out.println("test1111111");
         SpringApplication.run(App.class,args);
     }
 }
